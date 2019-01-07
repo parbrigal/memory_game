f7cab6f48dc320c73faaec1898771225b6ba35fb
Julius Heymans <jheymans@labournet.com>
2019-01-07 16:17:48 +0200

    Component States

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -3,20 +3,16 @@ import '../App.css';
 
 class MemoryCard extends Component {
 
-    state ={
-        flipped : false
-    }
-
-    flipCard = () => {
-      this.setState({
-        flipped : true
-      })
-      this.props.callbackFromParent(this);
-        setTimeout(() => {
-          this.setState({
-            flipped:false
-          });
-        },1500)
+    flipCard = (id) => {
+      // this.setState({
+      //   flipped : true
+      // })
+      this.props.callbackFromParent(id);
+        // setTimeout(() => {
+        //   this.setState({
+        //     flipped:false
+        //   });
+        // },1500)
       
   
     }
@@ -25,7 +21,7 @@ class MemoryCard extends Component {
 
   render() {
     return (
-        <div className= {this.props.flipped ? "memory-card flip" : "memory-card"} style={{width: 'calc(25% - 10px)',height: 'calc(33.333% - 10px)',boxShadow:'1px 1px 1px rgba(0,0,0,.3)',transformStyle: 'preserve-3d'}} onClick={() => this.flipCard()}>
+        <div className= {this.props.flipped ? "memory-card flip" : "memory-card"} style={{width: 'calc(25% - 10px)',height: 'calc(33.333% - 10px)',boxShadow:'1px 1px 1px rgba(0,0,0,.3)',transformStyle: 'preserve-3d'}} onClick={() => this.flipCard(this.props.id)}>
         <img className="front-face" src={`/images/${this.props.thmb}.svg`} alt="React" />
         <img className="back-face" src={'/images/js.svg'} alt="JavaScript" />
       </div>
--- src/components/MemoryGame.jsx
+++ src/components/MemoryGame.jsx
@@ -1,60 +1,87 @@
 import React, { Component } from "react";
-import MemCard from "./MemCard";
+import MemoryCard from "./MemoryCard";
 
 class MemoryGame extends Component {
   state = {
     cardList: [
+      { id: 0, cardType: "react", flipped: false },
       { id: 1, cardType: "react", flipped: false },
-      { id: 2, cardType: "react", flipped: false },
+      { id: 2, cardType: "angular", flipped: false },
       { id: 3, cardType: "angular", flipped: false },
-      { id: 4, cardType: "angular", flipped: false },
+      { id: 4, cardType: "aurelia", flipped: false },
       { id: 5, cardType: "aurelia", flipped: false },
-      { id: 6, cardType: "aurelia", flipped: false },
+      { id: 6, cardType: "backbone", flipped: false },
       { id: 7, cardType: "backbone", flipped: false },
-      { id: 8, cardType: "backbone", flipped: false },
+      { id: 8, cardType: "vue", flipped: false },
       { id: 9, cardType: "vue", flipped: false },
-      { id: 10, cardType: "vue", flipped: false },
-      { id: 11, cardType: "ember", flipped: false },
-      { id: 12, cardType: "ember", flipped: false }
+      { id: 10, cardType: "ember", flipped: false },
+      { id: 11, cardType: "ember", flipped: false }
     ],
     hasFlippedCard: false,
-    firstcard: "",
-    secondcard: ""
+    firstCard: null,
   };
 
   handleFlip = type => {
        const { cardList } = this.state;
        // 1. Make a shallow copy of the items
        let newCardListData = [ ...cardList ];
-       console.log(newCardListData)
-
-
         // 2. Make a shallow copy of the item you want to mutate
         let item = {...cardList[type]};
         // 3. Replace the property you're intested in
         item.flipped = true
         // 4. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
         newCardListData[type] = item 
-       // 5. Set the state to our new copy
-       //this.setState({cardList:newCardListData});
- /*    if (!this.state.hasFlippedCard) {
-      this.setState({
-        hasFlippedCard: true,
-        firstcard: type
-      });
-      return;
-    }
-    let secondcard = type;
-    this.checkForMatch(secondcard); */
+        // 5. Set the state to our new copy
+        this.setState({
+          cardList:newCardListData,
+        });
+
+        if (!this.state.hasFlippedCard)
+        {
+          this.setState({
+            hasFlippedCard:true,
+            firstCard: type
+          })
+        }
+        else {
+          this.checkForMatch(type)
+        }  
+        
   };
 
-  checkForMatch = secondcard => {
-    const { firstcard } = this.state;
+  checkForMatch = secondCard => {
+    const { firstCard,cardList } = this.state;
+    let newCardListData = [ ...cardList ];
+    let firstCardItem = {...cardList[firstCard]}
+    let secondCardItem = {...cardList[secondCard]}
 
-    if (firstcard.props.thmb === secondcard.props.thmb) {
-      console.log("matches");
+    if (firstCardItem.cardType === secondCardItem.cardType) {
+      console.log('matches')
+      /*
+        They match don't flip them back
+      */
+     console.log(firstCardItem);
+     console.log(secondCardItem);
     } else {
-      console.log("!matches");
+      /*
+        flip them back
+      */
+
+     firstCardItem.flipped = false;
+     newCardListData[firstCard] = firstCardItem
+
+     secondCardItem.flipped = false;
+     newCardListData[secondCard] = secondCardItem
+
+     console.log(newCardListData);
+     
+     setTimeout(() => {
+      this.setState({
+        cardList: newCardListData,
+        hasFlippedCard : false,
+        firstCard : null
+        })
+      }, 1000);
     }
   };
 
@@ -73,8 +100,9 @@ class MemoryGame extends Component {
         }}
       >
         {cardList.map(card => (
-          <MemCard
+          <MemoryCard
             key={card.id}
+            id={card.id}
             thmb={card.cardType}
             callbackFromParent={this.handleFlip}
             hasFlippedCard={this.state.hasFlippedCard}
